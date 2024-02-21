7a88725ba0624466549e2bb3970bef86dda658dc
numilani <numilani@protonmail.com>
2024-02-21 00:30:25 -0500

    init commit

---
+++ SimpleRPManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using NpgsqlTypes;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
+using Serilog.Sinks.PostgreSQL.ColumnWriters;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        // load in config file
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        IConfiguration config = builder.Build();
+
+        // Create the default logger
+
+        var logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(config)
+        .CreateLogger();
+
+        Log.Logger = logger;
+
+        logger.Error("Test!");
+
+        await logger.DisposeAsync();
+
+    }
+}
